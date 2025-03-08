5559d1262a58e1f2e5d8c90516830cb2af67c26c
Amrit <Amrit.09@icloud.com>
2025-03-07 19:52:39 -0800

    Vercel.json Updated II

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ const root = document.getElementById("root");
 
 async function fetchStockFinancials(symbol){
     try{
-        const response = await fetch(`/api/aplhaVantage/stock?symbol=${symbol}`);
+        const response = await fetch(`/api/alphaVantage/stock?symbol=${symbol}`);
         const data = await response.json();
         console.log(data);
         
