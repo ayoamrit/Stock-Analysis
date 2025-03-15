b21244e0a46c424c6c5a1527b29738213f80d950
Amrit Dhaliwal <Amrit78887@gmail.com>
2025-03-14 22:58:20 -0700

    Script Updated

--- public/script.js
+++ public/script.js
@@ -1,23 +1,38 @@
-import {getTableContainer} from "./table.js";
+import {getTable} from "./table.js";
 /**
  * Root
  */
 const root = document.getElementById("root");
 
 
+/**
+ * Fetch stock financial data from the backend API and generates financial tables
+ * 
+ * @param {string} symbol - Stock ticker symbol (e.g. "MSFT" for Microsoft) 
+ */
 async function fetchStockFinancials(symbol){
     try{
+
+        //Fetch financial data from the API
         const response = await fetch(`/api/alphaVantage/stock?symbol=${symbol}`);
         const data = await response.json();
         console.log(data);
         
+        //Append Balance Sheet Table if data exists
         if(data.balanceSheet && data.balanceSheet.annualReports){
-            const balanceSheetTable = getTableContainer(data.balanceSheet.annualReports, "balanceSheet");
-            const cashFlowStatementTable = getTableContainer(data.cashFlowStatement.annualReports, "cashFlowStatement");
-            const incomeStatementTable = getTableContainer(data.incomeStatement.annualReports, "incomeStatement");
-
+            const balanceSheetTable = getTable(data.balanceSheet.annualReports, "balanceSheet");
             root.appendChild(balanceSheetTable);
+        }
+
+        //Append Cash Flow Statement if data exists
+        if(data.cashFlowStatement && data.cashFlowStatement.annualReports){
+            const cashFlowStatementTable = getTable(data.cashFlowStatement.annualReports, "cashFlowStatement");
             root.appendChild(cashFlowStatementTable);
+        }
+
+        //Append Income Statement if data exists
+        if(data.incomeStatement && data.incomeStatement.annualReports){
+            const incomeStatementTable = getTable(data.incomeStatement.annualReports, "incomeStatement");
             root.appendChild(incomeStatementTable);
         }
     }catch(error){
